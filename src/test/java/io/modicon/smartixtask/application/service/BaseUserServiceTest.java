b25d2f77edc8c6dde7f02dea018dc0d070b85e28
saeed <rebeelsc@gmail.com>
2023-02-13 18:16:49 +0700

    add user management service test

---
+++ src/test/java/io/modicon/smartixtask/application/service/BaseUserServiceTest.java
@@ -0,0 +1,86 @@
+package io.modicon.smartixtask.application.service;
+
+import io.modicon.smartixtask.domain.model.UserEntity;
+import io.modicon.smartixtask.domain.repository.UserRepository;
+import io.modicon.smartixtask.infrastructure.exception.ApiException;
+import io.modicon.smartixtask.infrastructure.security.CustomUserDetails;
+import io.modicon.smartixtask.infrastructure.security.jwt.JwtGeneration;
+import io.modicon.smartixtask.web.dto.UserLoginResponse;
+import io.modicon.smartixtask.web.dto.UserRegisterRequest;
+import io.modicon.smartixtask.web.dto.UserRegisterResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class BaseUserServiceTest {
+
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private JwtGeneration jwtGeneration;
+
+    @Mock
+    private PhoneValidationService phoneValidationService;
+
+    @BeforeEach
+    void setUp() {
+        userService = new UserService.Base(userRepository, passwordEncoder, jwtGeneration, phoneValidationService);
+    }
+
+    @Test
+    void shouldRegisterUser() {
+        String telephone = "telephone";
+        String password = "password";
+
+        when(userRepository.existsById(telephone)).thenReturn(false);
+        when(phoneValidationService.isValidPhoneNumber(telephone, "RU")).thenReturn(true);
+        when(passwordEncoder.encode(password)).thenReturn(password);
+        String token = "token";
+        when(jwtGeneration.generateAccessToken(new CustomUserDetails(telephone, password))).thenReturn(token);
+
+        UserRegisterResponse result = userService.register(new UserRegisterRequest(telephone, password));
+
+        assertEquals(result.getTelephone(), telephone);
+        assertEquals(result.getToken(), token);
+    }
+
+    @Test
+    void shouldNotRegisterUser_whenTelephoneAlreadyExist() {
+        String telephone = "telephone";
+        String password = "password";
+
+        when(userRepository.existsById(telephone)).thenReturn(true);
+
+        assertThatThrownBy(() -> userService.register(new UserRegisterRequest(telephone, password)))
+                .isInstanceOf(ApiException.class)
+                .hasMessageContaining(String.format("user with telephone number [%s] already exist", telephone));
+    }
+
+    @Test
+    void shouldNotRegisterUser_whenTelephoneIsInvalid() {
+        String telephone = "telephone";
+        String password = "password";
+
+        when(userRepository.existsById(telephone)).thenReturn(false);
+        when(phoneValidationService.isValidPhoneNumber(telephone, "RU")).thenReturn(false);
+
+        assertThatThrownBy(() -> userService.register(new UserRegisterRequest(telephone, password)))
+                .isInstanceOf(ApiException.class)
+                .hasMessageContaining(String.format("phone number [%s] is invalid", telephone));
+    }
+
+}
